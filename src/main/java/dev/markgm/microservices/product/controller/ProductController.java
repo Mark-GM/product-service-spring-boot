fa6903a5d5f7b3e76d4f49d47b85cf9b4f02169e
Mark GM <markgamalm@gmail.com>
2025-09-07 05:35:15 +0300

    feat(controller): create ProductController class for HTTP handling of product-service

---
+++ src/main/java/dev/markgm/microservices/product/controller/ProductController.java
@@ -0,0 +1,32 @@
+package dev.markgm.microservices.product.controller;
+
+import dev.markgm.microservices.product.dto.ProductRequest;
+import dev.markgm.microservices.product.dto.ProductResponse;
+import dev.markgm.microservices.product.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getProducts() {
+        return productService.getAllProducts();
+    }
+
+}
