749ee064625b57e0eedd480c75039252ad00787a
Mark GM <markgamalm@gmail.com>
2025-09-20 02:56:07 +0300

    test(testcontainers): update testcontainers configuration to use mongo:8.0.12 image

--- src/test/java/dev/markgm/microservices/product/TestcontainersConfiguration.java
+++ src/test/java/dev/markgm/microservices/product/TestcontainersConfiguration.java
@@ -9,10 +9,13 @@ import org.testcontainers.utility.DockerImageName;
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-	@Bean
-	@ServiceConnection
-	MongoDBContainer mongoDbContainer() {
-		return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
-	}
+    @Bean
+    @ServiceConnection
+    MongoDBContainer mongoDbContainer() {
+        final DockerImageName mongoDbImage = DockerImageName
+                .parse("mongo:8.0.12@sha256:108c3c1646aa75e8f75361024ae3f90b2ba9d2ad6d442d042bdeb269ef8d15bf")
+                .asCompatibleSubstituteFor("mongo");
+        return new MongoDBContainer(mongoDbImage);
+    }
 
 }
