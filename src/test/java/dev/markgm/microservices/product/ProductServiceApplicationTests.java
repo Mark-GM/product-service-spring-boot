e70ffc48ff70a1140be52becdadd3106d982bc94
Mark GM <markgamalm@gmail.com>
2025-09-20 03:08:20 +0300

    test(product): verify adding a product via the API works

--- src/test/java/dev/markgm/microservices/product/ProductServiceApplicationTests.java
+++ src/test/java/dev/markgm/microservices/product/ProductServiceApplicationTests.java
@@ -1,15 +1,51 @@
 package dev.markgm.microservices.product;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// to use non-static method with @BeforeAll
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ProductServiceApplicationTests {
+    @LocalServerPort
+    private int port;
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeAll
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        // apache tomcat server port
+        RestAssured.port = port;
+    }
 
+    @Test
+    void shouldCreateProduct() {
+        String requestBody = """
+                {
+                  "name": "Galaxy S28 Ultra",
+                  "description": "Flagship android phone",
+                  "price": 87000
+                }
+                """;
+
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("api/v1/products")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.is("Galaxy S28 Ultra"))
+                .body("description", Matchers.is("Flagship android phone"))
+                .body("price", Matchers.is(87000));
+    }
 }
